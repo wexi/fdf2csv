c10d26b8c1f0fb8f1feeadef983fbc3e57533dd9
Enoch <ixew@hotmail.com>
2023-02-06 15:23:44 -0500

    bugfix

--- fdf2csv.py
+++ fdf2csv.py
@@ -63,8 +63,7 @@ except LookupError as e:
 # where magic happens
 pattern = re.compile(rb'<</T\((.*?)\)(?:(?:/V\((.*?)\))|(?:/V/(.*?)))?>>')
 finds = re.findall(pattern, fdf)
-fdf_list = [(find[0], find[2] if find[2] else find[3] if find[3] else b'')
-            for find in finds]
+fdf_list = [(find[0], find[2] if find[2] else find[3] if find[3] else b'') for find in finds]
 
 
 def oct(mat):
